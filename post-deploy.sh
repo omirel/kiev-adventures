d21c3e0880ea1d69041c6ce01ee0e0a51520e18f
Olaf Mirel <olaf@mirel.de>
2017-08-26 21:56:47 +0200

    colors from green to turkis

---
+++ post-deploy.sh
@@ -0,0 +1,7 @@
+rm -rf var/cache/*
+rm -rf var/logs/*
+rm -rf var/sessions/*
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var/cache var/logs var/uploads var/uploads/* web/uploads web/uploads/* var/indexes var/sessions
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var/cache var/logs var/uploads var/uploads/* web/uploads web/uploads/* var/indexes var/sessions
+echo done;
