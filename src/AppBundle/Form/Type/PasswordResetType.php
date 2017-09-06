9e0b6f54777a6ac665e076d13274893f7c35087f
Olaf Mirel <o.mirel@macrocom.de>
2017-09-06 15:08:59 +0200

    Added Password reset

---
+++ src/AppBundle/Form/Type/PasswordResetType.php
@@ -0,0 +1,45 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Sulu\Bundle\SecurityBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Create the password reset form type.
+ */
+class PasswordResetType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'plainPassword',
+            PasswordType::class,
+            [
+                'label' => 'Password',
+                'mapped' => false,
+            ]
+        );
+
+        $builder->add('submit', SubmitType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => User::class,
+                'validation_groups' => ['password_reset'],
+            ]
+        );
+    }
+}
